51aff6799d6c8d990657898dd44beaeecc94268f
Matthew Robinson <Matthew.m.robinson10@gmail.com>
2023-03-23 13:36:07 -0400

    Added Footer.jsx, fontAwesomeCDN

--- CLIENT/src/Components/Footer.jsx
+++ CLIENT/src/Components/Footer.jsx
@@ -6,10 +6,27 @@ const year = currentDate.getFullYear();
 function Footer() {
   return (
     <div className='footer'>
-      <p id='footer-text'>Created by 
-        <a href="https://github.com/Mattrob10/Travel-Gear"target="_blank">
-            <i className="fab fa-github"></i>
-          </a>Aloe, Kirk, & Matt &copy; {year}</p>
+      <div className="copyrite">
+        <h3 id="credit-title">Created by 
+          Aloe, Kirk, & Matt &copy; {year}</h3>
+          <h1><a href="https://github.com/Mattrob10/Travel-Gear"target="_blank">
+            <i className="fab fa-github" id="github-repo-link"></i>
+          </a></h1>
+      </div>
+       <div className="credits">
+        <h3 id="credit-title-right">Our GitHub</h3>
+        <div className="credit-info">
+          <h4>Aloe<a href="https://github.com/search?q=eonflower"target="_blank">
+            <i className="fab fa-github"id="github-icon"></i>
+          </a></h4>
+          <h4>Kirk <a href="https://github.com/kirksurber1"target="_blank">
+            <i className="fab fa-github"id="github-icon"></i>
+          </a></h4>
+          <h4>Matthew <a href="https://github.com/Mattrob10"target="_blank">
+            <i className="fab fa-github" id="github-icon"></i>
+          </a></h4>
+        </div>
+       </div>
     </div>
   );
 }
