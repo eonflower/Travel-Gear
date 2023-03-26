4af8b75ad6722439dda18c6b05453c0a150c06d8
Matthew Robinson <Mmrobinson1983@gmail.com>
2023-03-26 10:38:22 -0400

    updated branch from dev

--- CLIENT/src/Pages/HomePage.jsx
+++ CLIENT/src/Pages/HomePage.jsx
@@ -21,14 +21,14 @@ function HomePage(props) {
 
     <div className="techGear-slider">
       <h1 className="techGear-title">Tech Gear</h1>
-    <Slider dots={true} infinite={true} autoplay={true} autoplaySpeed={2000} speed={500} slidesToShow={3} slidesToScroll={1}>
+    <Slider dots={true} infinite={true} autoplay={true} autoplaySpeed={3000} speed={500} slidesToShow={3} slidesToScroll={1}>
   {techGear.map((item) => (
-    <a href="/techGear" className="item-container" key={item._id}>
+    <div className="item-container" key={item._id}>
       <img src={item.imgURL} alt={item.title} id='tech-gear-img' />
       <h1 className="item-brand">{item.brand}</h1>
       <h2 className="item-name">{item.name}</h2>
       {/* <p>{item.description.slice(0, 200)}{item.description.length > 200 ? "..." : ""}</p> */}
-    </a>
+    </div>
   ))}
   </Slider>
   </div>
@@ -41,14 +41,14 @@ function HomePage(props) {
 
   <div className="photoGear-slider">
     <h1 className="photoGear-title">Photography Gear</h1>
-    <Slider dots={true} infinite={true} autoplay={true} autoplaySpeed={2000} speed={500} slidesToShow={3} slidesToScroll={1}>
+    <Slider dots={true} infinite={true} autoplay={true} autoplaySpeed={3000} speed={500} slidesToShow={3} slidesToScroll={1}>
   {photoGear.map((item) => (
-    <a href="/photography" className="item-container" key={item._id}>
+    <div className="item-container" key={item._id}>
       <img className="gear-img" src={item.imgURL} alt={item.title} id='tech-gear-img' />
       <h1 className="item-brand">{item.brand}</h1>
       <h2 className="item-name">{item.name}</h2>
       {/* <p>{item.description.slice(0, 200)}{item.description.length > 200 ? "..." : ""}</p> */}
-    </a>
+    </div>
   ))}
 </Slider>
 </div>
