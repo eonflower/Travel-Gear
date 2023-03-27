adba7abd49d98a9cd76e67f647d5c6f35e1c34ce
Matthew Robinson <Mmrobinson1983@gmail.com>
2023-03-27 14:07:11 -0400

    techGearDetails updated

--- CLIENT/src/Pages/PhotographyDetails.jsx
+++ CLIENT/src/Pages/PhotographyDetails.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState} from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
 function PhotographyDetails()  {
   const {id} = useParams()
-  console.log(id)
+  const [item, setItem] = useState(null);
+
   const getData = () => {
     axios.get(`/api/photography/${id}`)
-    .then(res => console.log(res))
+    .then((res) => setItem(res.data))
     .catch(err => console.log(err))
   }
     
@@ -16,9 +17,21 @@ function PhotographyDetails()  {
   }, [])
 
   return (
-    <div>{id}
-   
+    <div>
+      {item ? (
+        <>
+          <h2>{item.brand}</h2>
+          <h2>{item.name}</h2>
+          <img src={item.imgURL} alt={item.title} />
+          <p>{item.description}</p>
+          <p>{item.capacity}</p>
+          <p>{item.price}</p>
+         {/* add more item properties as needed you have a TON on your API 👏 */}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  )
+  );
 }
 export default PhotographyDetails;
--- CLIENT/src/Pages/TechGearDetails.jsx
+++ CLIENT/src/Pages/TechGearDetails.jsx
@@ -1,24 +1,41 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
-function TechGearDetails()  {
-  const {id} = useParams()
-  console.log(id)
+function TechGearDetails() {
+  const { id } = useParams();
+  const [item, setItem] = useState(null); 
+
   const getData = () => {
-    axios.get(`/api/techGear/${id}`)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-  }
-    
+    axios
+      .get(`/api/techGear/${id}`)
+      .then((res) => setItem(res.data))
+      .catch((err) => console.log(err));
+  };
+
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   return (
-    <div>{id}
-   
+    <div>
+      {item ? (
+        <>
+          <h2 className='item-brand'>{item.brand}</h2>
+          <h3 className='item-name'>{item.name}</h3>
+          <h3 className='item-type'>
+            Type: {item.style} | ${item.price}
+          <h4 className='item-size'>Capacity: {item.capacity}L</h4>  
+          </h3>
+          <img src={item.imgURL} alt={item.title} />
+          <p className='item-description'>{item.description}</p>
+          
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  )
+  );
 }
+
 export default TechGearDetails;
