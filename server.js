af395b456fbaca9ce5447bfde3430619209fa55f
Aloe Nelson <aloe.eon@gmail.com>
2023-03-30 09:12:00 -0700

    test

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
+// const express = require("express")
+// const app = express()
+// const morgan = require("morgan")
+// const mongoose = require("mongoose")
+// require("dotenv").config();
+
+// const PORT = process.env.PORT || 9000;
+
+
+
+// //Connect to DB
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("Connection to database!");
+//   })
+//   .catch(dbErr => {
+//     console.log("Connection to database failed:", dbErr.message);
+//   });
+  
+
+
+
+// // MIDDLEWARE
+// app.use(express.json())
+// app.use(morgan('dev'))
+
+// // ROUTES
+// app.use("/techGear", require("./Routes/techGearRouter.js"))
+
+// app.use("/photography", require("./Routes/photographyRouter.js"))
+
+// app.use("/wishlist", require("./Routes/wishlistRouter.js"))
+
+
+// // ERROR HANDLER
+// app.use((err, req, res, next) => {
+//   console.log(err)
+//   res.send({errMsg: err.message})
+// })
+
+// app.listen(PORT, () => {
+//   console.log("Server is running on Port 9000")
+// })
+
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
 const mongoose = require("mongoose")
-require("dotenv").config();
-
-const PORT = process.env.PORT || 9000;
 
 
 
 //Connect to DB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://mattrob101:finn1212@cluster0.msb08b3.mongodb.net/?retryWrites=true&w=majority")
   .then(() => {
     console.log("Connection to database!");
   })
@@ -17,7 +58,6 @@ mongoose.connect(process.env.MONGO_URI)
     console.log("Connection to database failed:", dbErr.message);
   });
   
-
 
 
 // MIDDLEWARE
@@ -35,9 +75,9 @@ app.use("/wishlist", require("./Routes/wishlistRouter.js"))
 // ERROR HANDLER
 app.use((err, req, res, next) => {
   console.log(err)
-  return res.send({errMsg: err.message})
+  res.send({errMsg: err.message})
 })
 
-app.listen(PORT, () => {
+app.listen(9000, () => {
   console.log("Server is running on Port 9000")
 })
